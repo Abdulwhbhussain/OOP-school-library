e938e45c9a8ad1ea915440bd60b4a463bfd1aa17
WHB901 <abdulwhbhussain@gmail.com>
2023-08-28 21:27:32 +0500

    update readme file

--- classes/person.rb
+++ classes/person.rb
@@ -1,29 +1,14 @@
 # Person Class
 
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  # Getters and Setters
-
-  def id
-    @id
-  end
-  def name
-    @name
-  end
-  def age
-    @age
-  end
-  def name=(value)
-    @name = value
-  end
-  def age=(value)
-    @age = value
   end
 
   # Public Methods
