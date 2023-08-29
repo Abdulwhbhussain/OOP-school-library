abf8a9624f26a4b2ebce8c44fb0bcf5e12aa8154
WHB901 <abdulwhbhussain@gmail.com>
2023-08-30 01:06:06 +0500

    Decorator Add

---
+++ classes/decorator.rb
@@ -0,0 +1,28 @@
+require './nameable'
+
+# Base Decorator Class
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+# Prepare CapitalizeDecorator and TrimmerDecorator
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.strip
+  end
+end
