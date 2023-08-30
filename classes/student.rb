af538c70edb6cd4f5d2eedd65a9ac5e85104051e
WHB901 <abdulwhbhussain@gmail.com>
2023-08-30 23:19:38 +0500

    Add Associations

--- classes/student.rb
+++ classes/student.rb
@@ -1,11 +1,19 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(name, age, parent_permission, classroom)
     super(name, age, parent_permission)
     @classroom = classroom
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.member?(self)
+  end
+  
   def play_hooky
     '¯\(ツ)/¯'
   end
