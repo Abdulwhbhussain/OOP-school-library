91db7b7620490516eafaaf10f7b8e829048a2285
WHB901 <abdulwhbhussain@gmail.com>
2023-08-31 00:07:21 +0500

    Add Associations

--- classes/person.rb
+++ classes/person.rb
@@ -1,8 +1,9 @@
 require_relative 'nameable'
+require_relative 'rental'
 # Person Class
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -11,9 +12,14 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   # Public Methods
+  def add_rental(date, book)
+    rentals.push(Rental.new(date, book, self))
+  end
+
   def can_use_services?
     of_age? || @parent_permission
   end
