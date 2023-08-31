d4aa66509d1cedfb912af6275ba4bc8a929aa349
WHB901 <abdulwhbhussain@gmail.com>
2023-08-31 23:34:14 +0500

    Internal Structure

--- main.rb
+++ main.rb
@@ -1,5 +1,16 @@
 require_relative 'app'
 
+def show_menu
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
 # Create a new instance of the App class
 
 def main
@@ -8,14 +19,7 @@ def main
   puts ' '
   number = 0
   while number != 7
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
+    show_menu
     number = gets.chomp.to_i
     case number
     when 1
