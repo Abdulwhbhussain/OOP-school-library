3505c56b1d4898c15fbbe78d682540b0535d7ecc
WHB901 <abdulwhbhussain@gmail.com>
2023-10-03 13:15:13 +0500

    Basic UI

---
+++ main.rb
@@ -0,0 +1,85 @@
+require_relative 'app'
+
+def show_menu
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+def rentals
+  print 'ID of person: '
+  id = gets.chomp.to_i
+  app.rentals_of_person(id)
+end
+
+def main
+  app = App.new
+  puts 'Welcome to School Library App!'
+  puts ' '
+  number = 0
+  while number != 7
+    show_menu
+    number = gets.chomp.to_i
+    choices = {
+      1 => app.books,
+      2 => app.people,
+      3 => app.add_person,
+      4 => app.add_book,
+      5 => app.add_rental,
+      6 => rentals,
+      7 => puts('Thank you for using the School Library App!')
+    }
+    if choices[number].nil?
+      puts 'Try again, please'
+      next
+    else
+      choices[number]
+    end
+  end
+end
+
+main
+
+# def choices(number, app)
+#   case number
+#   when 1
+#     app.books
+#   when 2
+#     app.people
+#   when 3
+#     app.add_person
+#   when 4
+#     app.add_book
+#   when 5
+#     app.add_rental
+#   when 6
+#     print 'ID of person: '
+#     id = gets.chomp.to_i
+#     app.rentals_of_person(id)
+#   when 7
+#     puts 'Thank you for using the School Library App!'
+#   else
+#     puts 'Try again, please'
+#   end
+# end
+
+# # Create a new instance of the App class
+
+# def main
+#   app = App.new
+#   puts 'Welcome to School Library App!'
+#   puts ' '
+#   number = 0
+#   while number != 7
+#     show_menu
+#     number = gets.chomp.to_i
+#     choices(number, app)
+#   end
+# end
+
+# main
