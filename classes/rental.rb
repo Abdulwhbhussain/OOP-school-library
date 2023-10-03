a6ff1531614256e71aab4e4ed44156968cd792ab
WHB901 <abdulwhbhussain@gmail.com>
2023-10-03 07:15:17 +0500

    Associations Established

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,7 +1,7 @@
 class Rental
   attr_accessor :book, :person, :date
 
-  def initialize(date)
+  def initialize(date, person = nil, book = nil)
     @person = person
     @book = book
     @date = date
