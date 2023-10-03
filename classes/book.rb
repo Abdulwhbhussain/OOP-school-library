70b3c90fd6b941e818cf8c78d23e31caa66aabd8
WHB901 <abdulwhbhussain@gmail.com>
2023-10-03 15:02:09 +0500

    Basic UI

--- classes/book.rb
+++ classes/book.rb
@@ -1,6 +1,8 @@
+require_relative 'rental'
+
 class Book
-  attr_reader :rentals
   attr_accessor :title, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
@@ -8,8 +10,24 @@ class Book
     @rentals = []
   end
 
-  # def add_rental(rental)
-  #   @rentals << rental
-  #   rental.book = self
-  # end
+  def add_rental(date, person)
+    @rentals.push(Rental.new(date, self, person))
+  end
 end
+
+
+# class Book
+#   attr_reader :rentals
+#   attr_accessor :title, :author
+
+#   def initialize(title, author)
+#     @title = title
+#     @author = author
+#     @rentals = []
+#   end
+
+#   # def add_rental(rental)
+#   #   @rentals << rental
+#   #   rental.book = self
+#   # end
+# end
