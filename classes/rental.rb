3218955c157eb2517e31190f4aff7c1bd8d49c96
WHB901 <abdulwhbhussain@gmail.com>
2023-10-03 15:01:01 +0500

    Basic UI

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,11 +1,35 @@
-class Rental
-  attr_accessor :date, :book, :person
+require_relative 'book'
+require_relative 'person'
 
-  def initialize(date, person, book)
-    @person = person
-    person.rentals << self
-    @book = book
-    book.rentals << self
+class Rental
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    @person = person
+  end
+
+  def book=(book)
+    @book = book
+    @book.rentals.push(self) unless @book.rentals.member?(self)
+  end
+
+  def person=(person)
+    @person = person
+    @person.rentals.push(self) unless @person.rentals.member?(self)
   end
 end
+
+# class Rental
+#   attr_accessor :date, :book, :person
+
+#   def initialize(date, person, book)
+#     @person = person
+#     person.rentals << self
+#     @book = book
+#     book.rentals << self
+#     @date = date
+#   end
+# end
